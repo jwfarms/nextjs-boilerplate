ede4269880023aeb8a9d501ee1512ed50991dbf6
jwfarms <jwfarms77@gmail.com>
2026-01-15 15:02:19 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,15 +67,10 @@ export default function RootLayout({
     }),
   }}
 />
-
-      <body
-
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        <RegisterSW />
-
-      >
-        {children}
-      </body>
-    </html>
-  );
+<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  <RegisterSW />
+  {children}
+</body>
+</html>
+);
 }
