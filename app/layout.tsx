f6dd5dd662122aeb0552feac19466ac7ca5daffc
jwfarms <jwfarms77@gmail.com>
2026-01-15 14:54:46 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import RegisterSW from "@/components/RegisterSW";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -68,6 +69,8 @@ export default function RootLayout({
 />
 
       <body
+        <RegisterSW />
+
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
