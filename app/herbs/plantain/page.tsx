302a1d5c41caf9ebfa2cf2d65822909d2bcd83a8
jwfarms <jwfarms77@gmail.com>
2026-02-13 15:31:39 -0600

    Create page.tsx

---
+++ app/herbs/plantain/page.tsx
@@ -0,0 +1,9 @@
+import HerbPage, { generateMetadata as genMeta } from "../[slug]/page";
+
+export function generateMetadata() {
+  return genMeta({ params: { slug: "plantain" } });
+}
+
+export default function Page() {
+  return <HerbPage params={{ slug: "plantain" }} />;
+}
