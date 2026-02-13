c591208c38785a583170349a552933644321133c
jwfarms <jwfarms77@gmail.com>
2026-02-13 17:09:13 -0600

    Delete app/herbs/lemon-balm directory

--- app/herbs/lemon-balm/page.tsx
+++
@@ -1,9 +0,0 @@
-import HerbPage, { generateMetadata as genMeta } from "../[slug]/page";
-
-export function generateMetadata() {
-  return genMeta({ params: { slug: "lemon-balm" } });
-}
-
-export default function Page() {
-  return <HerbPage params={{ slug: "lemon-balm" }} />;
-}
