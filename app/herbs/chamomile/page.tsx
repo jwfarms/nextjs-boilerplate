788d7b993cda9694a97397da8aa1af75558ca65b
jwfarms <jwfarms77@gmail.com>
2026-02-13 15:21:56 -0600

    Update page.tsx

--- app/herbs/chamomile/page.tsx
+++ app/herbs/chamomile/page.tsx
@@ -1,9 +1,9 @@
 import HerbPage, { generateMetadata as genMeta } from "../[slug]/page";
 
 export function generateMetadata() {
-  return genMeta({ params: { slug: "basil" } });
+  return genMeta({ params: { slug: "chamomile" } });
 }
 
 export default function Page() {
-  return <HerbPage params={{ slug: "basil" }} />;
+  return <HerbPage params={{ slug: "chamomile" }} />;
 }
